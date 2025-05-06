949d6b0203a69bbf1208ee8b0af6006be2cd2f9d
Duccio Rocchini <ducciorocchini@gmail.com>
2025-05-06 12:43:33 +0200

    Update first_code.R

--- first_code.R
+++ first_code.R
@@ -10,3 +10,14 @@ setwd("C://utente/Desktop/") # caution!!!! use slah and not backslash
 
 mato = rast("Matogrosso_l5_1992219_lrg.jpg")
 plot(mato)
+
+# Bands:
+# band 1 = near infrared
+# band 2 = red
+# band 3 = green
+
+plotRGB(mato, r=2, g=3, b=1)
+plotRGB(mato, r=2, g=1, b=3)
+plotRGB(mato, r=1, g=2, b=3)
+
+
